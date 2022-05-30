d29dae5f426366d529eb75002deb60394c835f12
alandev2 <alan.santos@stone.com.br>
2022-05-30 00:07:06 -0300

    feat: implements auth and tasks routers

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template, request, redirect, jsonify
-from sqlalchemy import null
-from models import db, UserModel
-import json
+from flask import Flask, jsonify, request
+from models import db, UserModel, TaskModel
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+
+from utils import httpResponse
 
 app = Flask(__name__)
+
+app.config["JWT_EXPIRE_DAYS"] = 7
+app.config["SECRET_KEY"] = "123456"
+
+jwt = JWTManager(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -11,50 +17,150 @@ db.init_app(app)
 
 @app.before_first_request
 def create_table():
-    db.create_all()
- 
-@app.post("/users")
-def create():
-  email = request.json['email']
-  user = UserModel(email=email)
-  db.session.add(user)
-  db.session.commit()
-
-  return "OK"
+  db.create_all()
 
 @app.get('/users')
 def index():
   users = UserModel.query.all()
   return jsonify(users)
 
-@app.get('/users/<string:id>')
-def get(id):
-  user = UserModel.query.filter_by(id=id).first()
+@app.post("/auth/login")
+def authLogin():
+  username = request.json['username']
+  user = UserModel.query.filter_by(username=username).first()
+  if(not user):   
+    responseError = { "message": "Username not found"}
+    
+    return httpResponse(responseError, 401)
+    
+  jwtToken = create_access_token(identity=user.id)
+    
+  return httpResponse({ "token": jwtToken })
 
-  return jsonify(user)
 
-@app.put('/users/<string:id>')
-def update(id):
-  user = UserModel.query.filter_by(id=id).first()
+@app.post("/auth/register")
+def authRegister():
+  try:
+    username = request.json['username']
+    user = UserModel.query.filter_by(username=username).first()
+    if(user):
+      responseError = { "message": "username already exist"}
+    
+      return httpResponse(responseError, 400)
 
-  newEmail = request.json['new_email']
+    user = UserModel(username=username)
+    db.session.add(user)
+    db.session.commit()
 
-  user.email = newEmail
+    return httpResponse(user, 201)
+  except Exception as e:
+    print(e)
+    responseObject = { "message": "internal server error" }
+
+    return httpResponse(responseObject, 500)
+
+@app.get('/tasks')
+@jwt_required()
+def listTasks():
+  userId = get_jwt_identity()
+  tasks = TaskModel.query.filter_by(user_id=userId).all()
+
+  return httpResponse(tasks)
+
+@app.post('/tasks')
+@jwt_required()
+def createTask():
+  userId = get_jwt_identity()
+  user = UserModel.query.filter_by(id=userId).first()
+  if(not user):
+    responseError = { "message": "User does not exist" }
+    return httpResponse(responseError, 401)
+
+  task = TaskModel(
+    user_id = userId,
+    title = request.json['title'],
+    description = request.json['description'],
+    status = request.json['status']
+  )
+
+  db.session.add(task)
   db.session.commit()
 
-  return jsonify(user)
+  return httpResponse(task, 201)
 
-@app.delete('/users/<string:id>')
-def delete(id):
-  user = UserModel.query.filter_by(id=id).first()
-  if(not user):
-    print("NULL")
-    return "NAO ENCONTRADO"
+@app.put('/tasks/<string:id>')
+@jwt_required()
+def updateTask(id):
+  task = TaskModel.query.filter_by(id=id).first()
+  if(not task):
+    responseError = { "message": "Task not found" }
+    return httpResponse(responseError, 404)
+
+  userId = get_jwt_identity()
+
+  if(task.user_id != userId):
+    responseError = { "message": "Task not found" }
+    return httpResponse(responseError, 403)
+
+  task.title = request.json['title']
+  task.description = request.json['description']
+  task.status = request.json['status']
+  db.session.commit()
+
+  return httpResponse(task)
+
+@app.delete('/tasks/<string:id>')
+@jwt_required()
+def deleteTask(id):
+  task = TaskModel.query.filter_by(id=id).first()
+  if(not task):
+    responseError = { "message": "Task not found" }
+    return httpResponse(responseError, 404)
+
+  userId = get_jwt_identity()
+
+  if(task.user_id != userId):
+    responseError = { "message": "Task not found" }
+    return httpResponse(responseError, 403)
+
+
+  db.session.delete(task)
+  db.session.commit()
+
+  return httpResponse({}, 204)
+
+@app.get('/profile')
+@jwt_required()
+def userProfile():
+  userId = get_jwt_identity()
+
+  profile = UserModel.query.filter_by(id=userId).first()
+  tasks = TaskModel.query.filter_by(user_id=userId).all()
+
+  profile.tasks = tasks
+
+  return httpResponse(profile, 200)
+
+@app.put('/users')
+@jwt_required()
+def update():
+  userId = get_jwt_identity()
+  user = UserModel.query.filter_by(id=userId).first()
+
+  user.username = request.json['username']
+  db.session.commit()
+
+  return httpResponse(user, 200)
+
+@app.delete('/users')
+@jwt_required()
+def delete():
+  userId = get_jwt_identity()
+  user = UserModel.query.filter_by(id=userId).first()
 
   db.session.delete(user)
   db.session.commit()
 
-  return jsonify(user)
-   
+  return httpResponse({}, 204)
 
 app.run(host='localhost', port=5000)
--- models.py
+++ models.py
@@ -1,14 +1,31 @@
 from dataclasses import dataclass
 from app import Flask
 from flask_sqlalchemy import SQLAlchemy
- 
+
 server = Flask(__name__)
 db = SQLAlchemy(server)
- 
+
 @dataclass
 class UserModel(db.Model):
-    id: int
-    email: str
+  __tablename__ = "user"
 
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(200), unique=True)
+  id: str 
+  username: str
+
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(200), unique=True)
+
+@dataclass
+class TaskModel(db.Model):
+  __tablename__ = "task"
+
+  id: str
+  title: str
+  description: str
+  status: str
+
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(255))
+  description = db.Column(db.String(255))
+  status = db.Column(db.String(50))
+  user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
